f9bef86c8dfa0f7ecc232e8d723448e7e64d30f0
phamtrung1411 <trungpv398@gmail.com>
2021-04-28 22:27:01 +0700

    okok

--- src/main/java/project_techwiz2/springboot_techwiz2/controller/api/ProductApiController.java
+++ src/main/java/project_techwiz2/springboot_techwiz2/controller/api/ProductApiController.java
@@ -1,14 +1,22 @@
 package project_techwiz2.springboot_techwiz2.controller.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import project_techwiz2.springboot_techwiz2.config.exeption.ErrorDetails;
 import project_techwiz2.springboot_techwiz2.config.exeption.ResourceNotFoundException;
+import project_techwiz2.springboot_techwiz2.model.core.Menu;
+import project_techwiz2.springboot_techwiz2.model.core.Product;
 import project_techwiz2.springboot_techwiz2.model.dto.ProductDto;
 import project_techwiz2.springboot_techwiz2.repository.core.ProductRepository;
 import project_techwiz2.springboot_techwiz2.repository.dto.ProductDtoRepository;
 
 
+import javax.validation.Valid;
+import java.util.Date;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 
 @CrossOrigin(origins = "http://localhost:4200")
@@ -48,6 +56,24 @@ public class ProductApiController {
         ProductDto productDto = productDtoRepository.findById(id)
                 .orElseThrow(()->new ResourceNotFoundException("User not found with id :"+id));
         return productDto;
+    }
+
+    @RequestMapping(path = "",method = RequestMethod.POST)
+    public ResponseEntity<?> saveMenu(@Valid @RequestBody ProductDto productDto)
+    {
+        try{
+            productDto.setCreated(new Date());
+            productDto.setUpdated(new Date());
+            productDtoRepository.save(productDto);
+            return new ResponseEntity<ProductDto>(productDto, HttpStatus.OK);
+        }catch (NoSuchElementException e)
+        {
+
+        }
+        ErrorDetails errorDetails = new ErrorDetails();
+        errorDetails.setTimestamp(new Date());
+        errorDetails.setMessage("Add new unsuccessful");
+        return new ResponseEntity<ErrorDetails>(errorDetails, HttpStatus.NOT_FOUND);
     }
 
 
